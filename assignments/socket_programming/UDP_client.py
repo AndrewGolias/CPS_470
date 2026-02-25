55b128cf91992afc0b9d955814152d9a71bcb9d1
Andrew Golias <agolias53@gmail.com>
2026-02-25 18:36:50 -0500

    comments for submission
    Signed-off-by: Andrew Golias <agolias53@gmail.com>

--- assignments/socket_programming/UDP_client.py
+++ assignments/socket_programming/UDP_client.py
@@ -1,42 +1,53 @@
 import socket
 import sys
 
+# get values from command prompt input
 MSG = sys.argv[1]
-if MSG != "HELLO":
-    validMsg = False
-else:
-    validMsg = True
 SERVER_IP = sys.argv[2]
 SERVER_PORT = int(sys.argv[3])
 CONNECTION_ID = sys.argv[4]
 
+# validate HELLO and connection ID length
+if MSG != "HELLO" or len(CONNECTION_ID) != 4:
+    sys.exit(1)
+
 tries = 0 # track user input for new connection ID attempts (max 3 tries)
 
-while validMsg and tries <= 3: # allows for 3 inputs after initial failed attempt
+while tries <= 3: # allows for 3 inputs after initial failed attempt
     try:
+        # create new socket for connection attempt
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.settimeout(60)
+        sock.settimeout(5)
+
+        # build message to send to server
         message = f"{MSG} {CONNECTION_ID}"
         sock.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
 
         data, addr = sock.recvfrom(1024)
         reply = data.decode().split()
 
-        if reply[0] == "OK":
+        # check response from server
+        if reply[0] == "OK":    # successful connection; end program
             print(f"Connection established {reply[1]} {reply[2]} {reply[3]}")
             sock.close()
             sys.exit(0)
-        else:
+        elif reply[0] == "RESET":   # successful connection; connection ID already in use
             print(f"Connection Error {reply[1]}")
             sock.close()
+        else:   # unsuccessful connection
+            raise socket.timeout
     except socket.timeout:
         print(f"Connection Timeout")
+        sock.close()
     except Exception:
+        sock.close()
         break
 
     tries += 1
-    if tries <= 3:
+    if tries <= 3:  # reattempt entering ID; give additional attempts if length is not 4
         print("Enter a new connection ID")
         CONNECTION_ID = input(">>")
+        while len(CONNECTION_ID) != 4:
+            CONNECTION_ID = input(">>")
 
 print("Connection Failure")
--- assignments/socket_programming/UDP_server.py
+++ assignments/socket_programming/UDP_server.py
@@ -3,53 +3,57 @@ import sys
 import time
 import threading
 
+# get values from command prompt input
 SERVER_IP = sys.argv[1]
 SERVER_PORT = int(sys.argv[2])
 
+connections = {}  # dictionary pairing: id -> timestamp
+
+# handle client connection, validate connection ID and generate response
 def handle_client(sock, d, client_addr):
     connectionId = d.decode('utf-8').strip().split()[1]
-    if connectionId in connections:
+    if connectionId in connections:     # ID in use
         reply = f"RESET {connectionId}"
     else:
         connections[connectionId] = time.time()
         reply = f"OK {connectionId} {client_addr[0]} {client_addr[1]}"
-
+    # send response to client
     sock.sendto(reply.encode('utf-8'), client_addr)
 
-try:
-    # create socket for all UDP communication
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((SERVER_IP, SERVER_PORT))
-    sock.settimeout(1)
-except socket.timeout:
-    sys.exit()
+# create socket for all UDP communication
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+sock.settimeout(1)
 
-connections = {}  # dictionary pairing: id -> timestamp
 last_request_time = time.time()
 
 while True:
+    # get current time
     now = time.time()
+
     # remove expired IDs (older than 60 seconds)
     living_connections = {}
     for connId, connTime in connections.items():
         if (now - connTime) < 60:
             living_connections[connId] = connTime
     connections = living_connections
+
     # server idle timeout (5 minutes)
     if (now - last_request_time) > 300:
         break
 
+    # receive message from client and call handle_client()
     try:
         data, addr = sock.recvfrom(1024) # for small msgs (1KB dedicated): HELLO ID
         last_request_time = time.time()
-        # threading improved parallel performance
+        # threading for improved parallel performance
         threading.Thread(
             target=handle_client,
             args=(sock, data, addr)
         ).start()
 
     except socket.timeout:
-        pass
+        continue
     except KeyboardInterrupt:
         break
 
